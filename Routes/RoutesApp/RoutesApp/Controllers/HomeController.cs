3a8f447a4d5c4069413da253582576c8c847f482
TANK10LOL\Andrii <avsanin8@gmail.com>
2017-10-12 11:38:59 +0300

    Working with routes (Basic Level)

---
+++ Routes/RoutesApp/RoutesApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutesApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int id=1, string name="")
+        {
+            return View();
+        }
+        public string GetController()
+        {
+            string controller = RouteData.Values["controller"].ToString();
+            return controller;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
