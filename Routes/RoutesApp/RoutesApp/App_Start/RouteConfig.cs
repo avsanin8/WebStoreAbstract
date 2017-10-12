e79b49e6506c2af3cfe8b1cf0fb41745cf8489e6
TANK10LOL\Andrii <avsanin8@gmail.com>
2017-10-12 12:59:08 +0300

    Ignore routes

--- Routes/RoutesApp/RoutesApp/App_Start/RouteConfig.cs
+++ Routes/RoutesApp/RoutesApp/App_Start/RouteConfig.cs
@@ -12,24 +12,58 @@ namespace RoutesApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.IgnoreRoute("Home/Index/12"); // Ignore It works!!
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { id = @"\d{2}"});
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            //    constraints: new { id = @"\d{2}", myConstraint = new CustomConstraint("Home/Index/12")}); // "Home/Index/12" - didn't work
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            //    constraints: new { /*controller = "^H.*",*/id = @"\d{2}", /*two integer*/
+            //    /*httpMethod=new HttpMethodConstraint("GET")*/}); // Route restraint (ограничение маршрута)
 
             //routes.MapRoute(
             //    name: "Default2",
             //    url: "Ua{controller}/{action}/{id}",
             //    defaults: new { id = UrlParameter.Optional });
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}/{*catchall}", 
-                defaults: new {controller = "Home", action= "Index" ,id = UrlParameter.Optional });
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}/{*catchall}", 
+            //    defaults: new {controller = "Home", action= "Index" ,id = UrlParameter.Optional });
 
-            
+
             /*,
             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }*/
 
 
             //Route newRoute = new Route("{controller}/{action}", new MvcRouteHandler());
             //routes.Add(newRoute);
+        }
+
+        public class CustomConstraint : IRouteConstraint
+        {
+            private string uri;
+            public CustomConstraint(string uri)
+            {
+                this.uri = uri;
+            }
+            public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+            {
+                return !(uri == httpContext.Request.Url.AbsolutePath);
+            }
         }
     }
 }
--- Routes/RoutesApp/RoutesApp/Controllers/HomeController.cs
+++ Routes/RoutesApp/RoutesApp/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace RoutesApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(int id=1, string name="")
+        public ActionResult Index(int id)
         {
             return View();
         }
